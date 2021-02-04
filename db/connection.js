cf341438a1526776f676e89696ebb7c18e7a1081
Will <williamjamescook@aol.com>
2021-02-04 11:18:25 +0000

    fixing internal server error

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,19 @@
-const dbConfig =
-  process.env.NODE_ENV === 'production'
-    ? { client: 'pg', connection: process.env.DATABASE_URL }
-    : require('../knexfile');
+const ENV = process.env.NODE_ENV || "development";
+const knex = require("knex");
 
-module.exports = require('knex')(dbConfig);
+const dbConfig =
+  ENV === "production"
+    ? {
+        client: "pg",
+        connection: {
+          connectionString: process.env.DATABASE_URL,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+      }
+    : require("../knexfile");
+
+const connection = knex(dbConfig);
+
+module.exports = connection;
